6b93fc36ca0362965fcc9c4cb2a66e010fae5c20
Tapan Anand <70485285+tapananand95@users.noreply.github.com>
2020-11-11 11:49:27 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Sample Heading One',
+    date: new Date(2020, 9, 4),
+    length: 11,
+    snippet: `Sample paragraph that goes with heading one.`,
+  },
+  {
+    id: 2,
+    title: 'Sample Heading Two',
+    date: new Date(2019, 10, 22),
+    length: 5,
+    snippet: `Sample paragraph that goes with heading two.`,
+  },
+  {
+    id: 3,
+    title: 'Sample Heading Three With More Words ',
+    date: new Date(2018, 7, 11),
+    length: 5,
+    snippet: `Sample paragraph that goes with heading three.`,
+  },
+  {
+    id: 4,
+    title: 'Sample Heading Four With More Words & Numbers',
+    date: new Date(2015, 5, 4),
+    length: 5,
+    snippet: `Sample paragraph with numbers 1,2,3,4 that goes with heading four.`,
+  },
+]
+
+export default articles
